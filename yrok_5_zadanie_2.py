f70f24003c5d554bfc3909092a0f6f84f52693f2
dybagov <159829115+dybagov@users.noreply.github.com>
2024-02-14 22:57:09 +0300

    Add files via upload

--- yrok_5_zadanie_2.py
+++ yrok_5_zadanie_2.py
@@ -1,13 +1,21 @@
-minsumma = int(input("Минимальная сумма инвестиций: "))
-mike = int(input("Сумма у Майка: "))
-ivan = int(input("Сумма у Ивана: "))
-if mike >= minsumma and ivan >= minsumma:
-    print("2")
-elif mike >= minsumma and ivan < minsumma:
-    print("Mike")
-elif ivan >= minsumma and mike < minsumma:
-    print("Ivan")
-elif mike + ivan >= minsumma:
-    print("1")
+word = input("Введите слово из маленьких латинских букв: ")
+# Списки гласных и согласных букв
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+# Переменные для хранения количеств гласных и согласных букв
+vowel_count = 0
+consonant_count = 0
+# Прохожу по каждой букве в слове
+for letter in word:
+    # Проверяю, является ли буква гласной
+    if letter.lower() in vowels:
+        vowel_count += 1
+    # Проверяю, является ли буква согласной
+    elif letter.lower() in consonants:
+        consonant_count += 1
+    # Если буква не является ни гласной, ни согласной, вывожу False
+    else:
+        print(False)
 else:
-    print("0")
+    print("Количество гласных букв:", vowel_count)
+    print("Количество согласных букв:", consonant_count)
